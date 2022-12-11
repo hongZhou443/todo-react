86997f042d923bf009ba6abffbf09ff295a57616
Hong Zhou <67214990+hongZhou443@users.noreply.github.com>
2022-12-10 18:16:47 -0600

    Update App.js
    For task description

--- src/App.js
+++ src/App.js
@@ -1,77 +1,82 @@
 import React, { useState, useRef, useEffect } from "react";
+import { nanoid } from "nanoid"; // npm install nanoid
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function usePrevious(value) {
   const ref = useRef();
   useEffect(() => {
     ref.current = value;
   });
+
   return ref.current;
 }
 
-const FILTER_MAP = {
-  All: () => true,
-  Active: task => !task.completed,
-  Completed: task => task.completed
-};
-
-const FILTER_NAMES = Object.keys(FILTER_MAP);
-
 function App(props) {
+
   const [tasks, setTasks] = useState(props.tasks);
-  const [filter, setFilter] = useState('All');
+  const [filter, setFilter] = useState("All");
 
   function toggleTaskCompleted(id) {
-    const updatedTasks = tasks.map(task => {
-      // if this task has the same ID as the edited task
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same id as the edited task
       if (id === task.id) {
-        // use object spread to make a new obkect
-        // whose `completed` prop has been inverted
-        return {...task, completed: !task.completed}
+        // use object spread to make a new object
+        // whose 'completed' prop has been inverted
+        return { ...task, completed: !task.completed }
       }
+
       return task;
-    });
+    })
     setTasks(updatedTasks);
   }
 
+  function addTask(name, description) {
+    const newTask = { id: `todo-${nanoid()}`, name: name, description: description, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function editTask(id, newName, newDescription) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same id as the edited task
+      if (id === task.id) {
+        return { ...task, name: newName, description: newDescription }
+      }
+      return task
+    })
+    setTasks(editedTaskList)
+  }
 
   function deleteTask(id) {
-    const remainingTasks = tasks.filter(task => id !== task.id);
+    const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
+    // console.log(`removed task ${id}`);
   }
 
-
-  function editTask(id, newName) {
-    const editedTaskList = tasks.map(task => {
-    // if this task has the same ID as the edited task
-      if (id === task.id) {
-        //
-        return {...task, name: newName}
-      }
-      return task;
-    });
-    setTasks(editedTaskList);
-  }
-
-  const taskList = tasks
-  .filter(FILTER_MAP[filter])
-  .map(task => (
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => (
     <Todo
       id={task.id}
       name={task.name}
+      description={task.description}
       completed={task.completed}
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
       editTask={editTask}
+      deleteTask={deleteTask}
     />
   ));
 
-  const filterList = FILTER_NAMES.map(name => (
+  const filterList = FILTER_NAMES.map((name) =>
+  (
     <FilterButton
       key={name}
       name={name}
@@ -80,33 +85,31 @@ function App(props) {
     />
   ));
 
-  function addTask(name) {
-    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
-    setTasks([...tasks, newTask]);
-  }
-
-
-  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
-  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  const taskNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
 
   const listHeadingRef = useRef(null);
-  const prevTaskLength = usePrevious(tasks.length);
+  const previousTaskLength = usePrevious(tasks.length);
 
   useEffect(() => {
-    if (tasks.length - prevTaskLength === -1) {
+    if(tasks.length - previousTaskLength === -1) {
       listHeadingRef.current.focus();
     }
-  }, [tasks.length, prevTaskLength]);
+  }, [tasks.length, previousTaskLength]);
 
   return (
     <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
       <Form addTask={addTask} />
+
       <div className="filters btn-group stack-exception">
         {filterList}
       </div>
+
       <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
         {headingText}
       </h2>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
@@ -117,5 +120,6 @@ function App(props) {
     </div>
   );
 }
+
 
 export default App;
