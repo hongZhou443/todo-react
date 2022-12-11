a76d9cad2c1775cda361dcfbcfcb899b742bbef6
Hong Zhou <67214990+hongZhou443@users.noreply.github.com>
2022-12-10 18:19:21 -0600

    Update index.js
    For task description

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
-];
+    { id: "todo-0", name: "Eat", description: "Dinner", completed: false },
+    { id: "todo-1", name: "Sleep", description: "8 hours", completed: false },
+    { id: "todo-2", name: "Repeat", description: "Every day", completed: false }
+]
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <App tasks={DATA} />
-  </React.StrictMode>,
-  document.getElementById('root')
 );
+
+// ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
